724799c1c868ecb880a4d6df7a741cb62b1b3b1c
Wridzer Kamphuis <wridzer12@gmail.com>
2024-05-01 23:18:07 +0200

    specular

--- FractalShader.glsl
+++ FractalShader.glsl
@@ -9,15 +9,28 @@ in mat3 TBN;
 uniform sampler2D mainTex;
 uniform sampler2D normalTex;
 uniform vec3 lightPosition;
+uniform vec3 cameraPosition;
+
 
 void main()
 {
     // Normalmap
-    vec3 normal = (texture(normalTex, uv).rgb * 2.0 - 1.0) * TBN;
+    vec3 normal = texture(normalTex, uv).rgb;
+    normal = normalize(normal * 2.0 - 1.0);
+    normal = normal * TBN;
 
     // Light
     vec3 lightDir = normalize(lightPosition - pixelCoord);
     float lightValue = max(dot(normal, lightDir), 0.0f);
 
-    FragColor = vec4(color, 1.0f) * texture(mainTex, uv) * min(lightValue + 0.1, 1.0);
+    // Specular data
+    vec3 viewDir = normalize(pixelCoord - cameraPosition);
+    vec3 reflectedLight = normalize(-reflect(-lightDir, normal));
+    float specValue = pow(max(dot(reflectedLight, viewDir), 0.0), 16);
+    
+    // Seperate RGB and RGBA
+    vec4 output = vec4(color, 1.0f) * texture(mainTex, uv);
+    output.rgb = output.rgb * min(lightValue + 0.1, 1.0) + specValue;
+
+    FragColor = output;
 }
